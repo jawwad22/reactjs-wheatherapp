7bab7d81a8c7843d08965a41519075f00d450694
unknown <mjawwadiqbal22@gmail.com>
2019-01-19 00:59:27 -0800

    --Error Handling

--- src/App.js
+++ src/App.js
@@ -4,44 +4,58 @@ import Titles from './components/Titles'
 import Forms from './components/Form'
 import Wheather from './components/Wheather'
 
-const API_KEY="6dc71c2e81850a1ea5be9adef9e62e0e"
+const API_KEY = "6dc71c2e81850a1ea5be9adef9e62e0e"
 
 class App extends React.Component {
-    state={
-        temperature:undefined,
-        city:undefined,
-        humdidity:undefined,
-        desription:undefined,
-        error:undefined,
+    state = {
+        temperature: undefined,
+        city: undefined,
+        humdidity: undefined,
+        desription: undefined,
+        error: undefined,
     }
-    getWeather= async (e) => {
+    getWeather = async (e) => {
         e.preventDefault()
-        const city=e.target.elements.city.value;
-        const country=e.target.elements.country.value;
-        const api_call= await fetch(
-            'http://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&APPID='+API_KEY
+        const city = e.target.elements.city.value;
+        const country = e.target.elements.country.value;
+        const api_call = await fetch(
+            'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&APPID=' + API_KEY
         );
-        const data =await api_call.json();
+        const data = await api_call.json();
 
         console.log(data);
-        this.setState({
-            temperature:data.main.temp,
-            humdidity:data.main.humidity,
-            city:data.name,
-            desription:data.weather[0].description
-        })
+        if (city === true) {
+            this.setState({
+                temperature: data.main.temp,
+                humdidity: data.main.humidity,
+                city: data.name,
+                desription: data.weather[0].description,
+                error:undefined
+            })
+        }
+        else
+        {
+            this.setState({
+                temperature: undefined,
+                humdidity: undefined,
+                city: undefined,
+                desription: undefined,
+                error:'Please entry the value in city'
+            })
+        }
     }
     render() {
         return (
             <div>
                 <Titles />
-                <Forms getWeather={this.getWeather}/>
+                <Forms getWeather={this.getWeather} />
                 <Wheather temperature={this.state.temperature}
-                city={this.state.city}
-                humidity={this.state.humdidity}
-                desription={this.state.desription}
+                    city={this.state.city}
+                    humidity={this.state.humdidity}
+                    desription={this.state.desription}
+                    error={this.state.error}
 
-                 />
+                />
             </div>
         );
     }
--- src/components/Wheather.js
+++ src/components/Wheather.js
@@ -4,15 +4,28 @@ class Wheather extends React.Component {
     render() {
         return (
             <div>
-            
-           {this.props.city}
-           {this.props.temperature}
-           {this.props.humidity}
-           {this.props.desription}
-           
-           
-           
-           
+                {
+                    this.props.city
+                    &&
+                    <div>
+                    <p> Location: {this.props.city}</p>
+                    <p> Temperature:{this.props.temperature}</p>
+                    <p> Humidity: {this.props.humidity}</p>
+                    <p> Description: {this.props.desription}</p>
+                    </div>
+            }
+            {
+                this.props.error!=undefined
+                &&
+                <div>
+                    <p>Error:{this.props.error}</p>
+                </div>
+                
+            }
+
+
+
+
             </div>
         );
     }
